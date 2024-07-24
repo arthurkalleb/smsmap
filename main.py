cd08398eaf0fedc88688f45ceb505ad2081abdf3
Kalleb <78311706+arthurkalleb@users.noreply.github.com>
2024-07-24 06:19:45 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import folium
+from folium.plugins import LocateControl
+
+# Cria o mapa com as coordenadas e o estilo desejado
+map = folium.Map(
+    location=[-5.664558763806402, -36.60399399589964],
+    tiles="OpenStreetMap",  # Corrigido para 'tiles'
+    zoom_start=5
+)
+
+# Adiciona o LocateControl ao mapa
+LocateControl(
+    auto_start=True,
+    drawMarker=True,  # Adiciona o marcador no mapa
+    markerStyle=dict(
+        className="leaflet-control-locate-marker",
+        color="green",
+        fillColor="black",
+        fillOpacity=1
+    )
+).add_to(map)
