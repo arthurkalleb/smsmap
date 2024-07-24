83e506f461bf11d3a3e4b994113ffe12eaaab0fa
Kalleb <78311706+arthurkalleb@users.noreply.github.com>
2024-07-24 14:10:55 -0300

    localizacao

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import streamlit as st
 import folium
-from folium.plugins import LocateControl
+from streamlit_folium import st_folium
 
 map = folium.Map([-5.6645401109365805, -36.60398193330714],
                  tiles="cartodbpositron", zoom_start=10)
-
 folium.plugins.LocateControl().add_to(map)
-
 folium.plugins.LocateControl(auto_start=True).add_to(map)
 
-map
+# Configura o Streamlit
+st.title("Mapa com Localização Atual")
+
+# Exibe o mapa no Streamlit
+st_data = st_folium(map, width=700, height=500)
